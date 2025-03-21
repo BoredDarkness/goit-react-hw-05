0b4344ff78d95689da6b6d9973e1654cde664c13
BoredDarkness <gwinnblaidd@gmail.com>
2025-03-21 10:29:14 +0100

    useSearchParams+useEffect

--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -1,26 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-import MovieList from "../../components/MovieList/MovieList.jsx";
+import MovieList from "../../components/MovieList/MovieList";
+import { useSearchParams } from "react-router-dom";
 import styles from "./MoviesPage.module.css";
 
 const API_BASE = "https://api.themoviedb.org/3";
 
 function MoviesPage() {
-  const [query, setQuery] = useState("");
   const [movies, setMovies] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const queryParam = searchParams.get("query") || "";
+  const [query, setQuery] = useState(queryParam);
 
-  const handleChange = (e) => {
-    setQuery(e.target.value);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (query.trim() === "") return;
+  useEffect(() => {
+    if (queryParam === "") {
+      setMovies([]);
+      return;
+    }
 
     axios
       .get(`${API_BASE}/search/movie`, {
         params: {
-          query,
+          query: queryParam,
           include_adult: false,
           language: "en-US",
           page: 1,
@@ -37,6 +38,19 @@ function MoviesPage() {
       .catch((error) => {
         console.error(error);
       });
+  }, [queryParam]);
+  const handleChange = (e) => {
+    setQuery(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (query.trim() === "") {
+      setSearchParams({});
+      setMovies([]);
+      return;
+    }
+    setSearchParams({ query });
   };
 
   return (
